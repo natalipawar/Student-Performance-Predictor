7cc691b13677c26fd6b8db6625093a40c19f0808
Natali Pawar <natalipawar@gmail.com>
2026-09-23 23:55:34 +0530

    model trained

--- train_model.py
+++ train_model.py
@@ -1,14 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-import pickle
 
 data = pd.read_csv("data/students.csv")
 
-print(data)
-
 X = data[["study_hours", "attendance", "previous_score"]]
-
 y = data["result"]
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -22,10 +18,4 @@ model = DecisionTreeClassifier(random_state=42)
 
 model.fit(X_train, y_train)
 
-accuracy = model.score(X_test, y_test)
-
-print("Accuracy:", accuracy)
-
-prediction = model.predict([[6, 85, 75]])
-
-print("Prediction:", prediction)
+print("Model trained!")
