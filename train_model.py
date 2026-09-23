25587974ad3ef53d1391ae1672f624a747782a25
Natali Pawar <natalipawar@gmail.com>
2026-09-23 23:47:03 +0530

    training model

--- train_model.py
+++ train_model.py
@@ -19,3 +19,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 model = DecisionTreeClassifier(random_state=42)
+
+model.fit(X_train, y_train)
+
+accuracy = model.score(X_test, y_test)
+
+print("Accuracy:", accuracy)
+
+prediction = model.predict([[6, 85, 75]])
+
+print("Prediction:", prediction)
